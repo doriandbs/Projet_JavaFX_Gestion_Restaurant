18e9bbb188c47a2138505baea991967088d7fd45
Dorian <dubois.dorian2@gmail.com>
2022-11-10 16:45:54 +0100

    Amelio login page

--- src/Main/java/App.java
+++ src/Main/java/App.java
@@ -10,18 +10,21 @@ import javafx.scene.layout.AnchorPane;
 
 import javafx.stage.Stage;
 
+
 import java.util.Objects;
 
 public class App extends Application {
-    /* public static void main(String[] args){
-    }*/
-
+    public static void main(String[] args) {
+    }
     public void start(Stage stage) throws Exception {
         AnchorPane pan = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Main/resources/fxml/login_page.fxml")));
         Scene scene = new Scene(pan);
         stage.setTitle("Gestionnaire de restaurant");
         stage.setScene(scene);
         stage.show();
-
     }
+
 }
+
+
+
--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,8 @@ module Projet.Java.Fx {
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
+    requires java.sql;
     exports Main.java to javafx.graphics;
+
 
 }
