08f4e215f60dc3fa76d5a740f7265957fbe73ac7
kevin <kevinlompo12@gmail.com>
2022-11-12 05:17:37 +0100

    update

--- src/Main/java/controller/InscriptionPageController.java
+++ src/Main/java/controller/InscriptionPageController.java
@@ -1,14 +1,11 @@
 package Main.java.controller;
 
 import Main.java.ConnectionClass;
-import javafx.event.ActionEvent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import java.sql.*;
-import java.sql.ResultSet;
+
 
 public class InscriptionPageController {
 
@@ -19,19 +16,16 @@ public class InscriptionPageController {
 
     public void addUser() {
         try {
-            //Statement statement = connection.createStatement();
             ConnectionClass conn = new ConnectionClass();
             Connection connection = conn.getConnection();
             PreparedStatement requete = connection.prepareStatement("insert into user(NOM,PRENOM,PASSWORD) values(?,?,?)");
             requete.setString(1, input_nom.getText().toUpperCase());
             requete.setString(2, input_prenom.getText().toUpperCase());
             requete.setString(3, input_psw.getText().toUpperCase());
-//            ResultSet resultSet;
             int n = requete.executeUpdate();
             System.out.println(n);
-            connection.close();
             requete.close();
-
+            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/Main/java/controller/LoginController.java
+++ src/Main/java/controller/LoginController.java
@@ -13,6 +13,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.Objects;
 
 public class LoginController {
     public TextField input_nom;
@@ -20,9 +21,8 @@ public class LoginController {
     public Label isConnected;
     Stage stage;
     Scene scene;
-    public void login(ActionEvent actionEvent){
+    public void login(){
         try {
-            //Statement statement = connection.createStatement();
             ConnectionClass conn = new ConnectionClass();
             Connection connection = conn.getConnection();
             PreparedStatement  requete = connection.prepareStatement("SELECT * FROM user WHERE NOM like ? AND PASSWORD like ? ");
@@ -30,18 +30,27 @@ public class LoginController {
             requete.setString(2, input_psw.getText());
             ResultSet resultSet = requete.executeQuery();
             while (resultSet.next()){
-                System.out.println(resultSet.getString("NOM") + " est connecté");
-                isConnected.setText("CONNEXION REUSSIE");
+                if (Objects.equals(input_nom.getText(), null) || Objects.equals(input_psw.getText(), "")) {
+                    isConnected.setText("VEUILLEZ RENTRER TOUT LES CHAMPS");
+                }
+                if (Objects.equals(resultSet.getString("NOM"), input_nom.getText()) && Objects.equals(resultSet.getString("PASSWORD"), input_psw.getText())){
+                    System.out.println(resultSet.getString("NOM") + " est connecté");
+                    isConnected.setText("CONNEXION REUSSIE");
+                }
+                if (!Objects.equals(resultSet.getString("NOM"), input_nom.getText()) || !Objects.equals(resultSet.getString("PASSWORD"), input_psw.getText())){
+                    isConnected.setText("NOM OU MOT DE PASSE INCORRECT");
+                }
             }
-            connection.close();
             requete.close();
+            connection.close();
+
         }catch(Exception e){
             e.printStackTrace();
         }
     }
     public void inscription(ActionEvent event) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/Main/resources/fxml/register_page.fxml"));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Main/resources/fxml/register_page.fxml")));
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
