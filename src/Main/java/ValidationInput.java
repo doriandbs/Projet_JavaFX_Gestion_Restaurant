77851f82f232805efb8f5164d46228e8be618f26
kevin <kevinlompo12@gmail.com>
2022-11-14 13:21:24 +0100

    ajout d'un message d'erreur si l'utilisateur n'existe pas dans la base de donnée

--- src/Main/java/ValidationInput.java
+++ src/Main/java/ValidationInput.java
@@ -5,12 +5,7 @@ import javafx.scene.control.TextField;
 public class ValidationInput {
 
     public static boolean textFieldNull(TextField inputTextField) {
-        boolean isNull = false;
-        String validationString = null;
-        if (inputTextField.getText().isEmpty()) {
-            isNull = true;
-        }
-        return isNull;
+        return inputTextField.getText().isEmpty();
     }
 }
 
