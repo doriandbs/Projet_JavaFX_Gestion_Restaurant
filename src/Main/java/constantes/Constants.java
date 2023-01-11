4d5a087efe2f92941bfb7a8d1ed24e101131dbd2
kevin <kevinlompo12@gmail.com>
2023-01-11 23:27:51 +0100

    add final in Constants class

--- src/Main/java/constantes/Constants.java
+++ src/Main/java/constantes/Constants.java
@@ -1,21 +1,21 @@
 package Main.java.constantes;
 public class Constants {
-    public static String nomRec = "Nom requis";
+    public static final String nomRec = "Nom requis";
 
-    public static String prenomRec = "Prénom requis";
-    public static String pswRec = "Mot de passe requis";
+    public static final String prenomRec = "Prénom requis";
+    public static final String pswRec = "Mot de passe requis";
 
-    public static String pswRegister = "Le mot de passe doit contenir au moins 8 caractères"
+    public static final String pswRegister = "Le mot de passe doit contenir au moins 8 caractères"
             +"\n"+"Au moins un chiffre"+"\n"+"Au moins une minuscule et une majuscule"+"\n"+"Au moins un caractère spécial et aucun espace";
-    public static String connSucc = "CONNEXION REUSSIE";
-    public static String verifCh = "veuillez rentrer tous les champs";
-    public static String  nomOrpswI = "nom ou mot de passe incorrect";
-    public static String userNotFound = "Utilisateur inconnu, veuilez cliquer vous inscrire";
-    public static String userExist = "L'utilisateur existe déjà";
-    public static String userCreat = "Utilisateur créer";
-    public static String user = "root";
-    public static String psw = "";
-    public static  String url = "jdbc:mysql://localhost:3306/projetrestaurant";
-    public static String driver = "com.mysql.cj.jdbc.Driver";
+    public static final String connSucc = "CONNEXION REUSSIE";
+    public static final String verifCh = "veuillez rentrer tous les champs";
+    public static final String  nomOrpswI = "nom ou mot de passe incorrect";
+    public static final String userNotFound = "Utilisateur inconnu, veuilez cliquer vous inscrire";
+    public static final String userExist = "L'utilisateur existe déjà";
+    public static final String userCreat = "Utilisateur créer";
+    public static final String user = "root";
+    public static final String psw = "";
+    public static final String url = "jdbc:mysql://localhost:3306/projetrestaurant";
+    public static final String driver = "com.mysql.cj.jdbc.Driver";
 
 }
