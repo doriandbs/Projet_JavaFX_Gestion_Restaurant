272e7cd00bebd814da5b46a38bc093ee2118e2f8
Dorian <dubois.dorian2@gmail.com>
2023-02-04 17:57:30 +0100

    Modification Database -> Design pattern Singleton
    Signed-off-by: Dorian <dubois.dorian2@gmail.com>

--- src/Main/java/controller/LoginController.java
+++ src/Main/java/controller/LoginController.java
@@ -1,6 +1,6 @@
 package Main.java.controller;
 
-import Main.bdd.ConnectionClass;
+import Main.bdd.DatabaseSingleton;
 import Main.java.ValidationInput;
 import Main.java.constantes.Constants;
 import javafx.event.ActionEvent;
@@ -13,7 +13,6 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Objects;
@@ -32,19 +31,19 @@ public class LoginController {
     Scene scene;
 
     public void login() {
-        boolean nom = ValidationInput.textFieldNull(input_badge);
+        boolean badge = ValidationInput.textFieldNull(input_badge);
         boolean password = ValidationInput.textFieldNull(input_psw);
 
-        if (nom && password) errormsg.setText(Constants.verifCh);
-        else if (nom) errormsg.setText(Constants.nomRec);
+        if (badge && password) errormsg.setText(Constants.verifCh);
+        else if (badge) errormsg.setText(Constants.badgeRec);
         else if (password) errormsg.setText(Constants.pswRec);
         else errormsg.setText(Constants.userNotFound);
-        if(!nom && !password) {
+        if (!badge && !password) {
 
             try {
-                ConnectionClass conn = new ConnectionClass();
-                Connection connection = conn.getConnection();
-                PreparedStatement requete = connection.prepareStatement("SELECT * FROM user WHERE BADGE = ? AND PASSWORD = ? ");
+                DatabaseSingleton db = DatabaseSingleton.getInstance();
+                db.connect();
+                PreparedStatement requete = db.prepareStatement("SELECT * FROM user WHERE BADGE = ? AND PASSWORD = ? ");
 //                System.out.println(checkPassword(input_badge.getText(),input_psw.getText()));
                 String pswHash = generateHash(input_psw.getText());
                 System.out.println(pswHash);
@@ -55,6 +54,7 @@ public class LoginController {
 
                 while (resultSet.next()) {
                     if (Objects.equals(resultSet.getString("BADGE"), input_badge.getText()) && Objects.equals(resultSet.getString("PASSWORD"), pswHash)) {
+                        //rajouter condition if si ISADMIN 1 ou 0
                         System.out.println(resultSet.getString("NOM") + "badge : " + resultSet.getString("BADGE") + " est connecté");
                         isConnected.setText(Constants.connSucc);
                         isConnected.setTextFill(Color.GREEN);
@@ -64,7 +64,7 @@ public class LoginController {
                     }
                 }
                 requete.close();
-                connection.close();
+                db.close();
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -84,27 +84,5 @@ public class LoginController {
         }
 
     }
-   /* public static boolean checkPassword(TextField username, PasswordField password) throws NoSuchAlgorithmException {
-        String hashedPassword = generateHash(password.toString());
-        System.out.println(hashedPassword);
-
-        try {
-            ConnectionClass conn = new ConnectionClass();
-            Connection connection = conn.getConnection();
-            PreparedStatement statement = connection.prepareStatement("SELECT password FROM user WHERE nom = ?");
-            statement.setString(1, username.toString());
-            System.out.println(username);
-            ResultSet resultSet = statement.executeQuery();
-            System.out.println(hashedPassword);
-            if (resultSet.next()) {
-                return hashedPassword.equals(resultSet.getString("password"));
-            } else {
-                return false;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }*/
 
 }
